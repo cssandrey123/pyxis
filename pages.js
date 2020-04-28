7d98c1b6bed6e5c9f6330024dc4b83a8c15de605
Ispas Andrei <andrei_ispas@yahoo.com>
2020-04-28 14:10:15 +0300

    modified login.html and pages.js

--- pages.js
+++ pages.js
@@ -10,7 +10,18 @@ const user = new User();
 //if the user submit login form
 router.post('/login', (req, res, next) => {
     //res.json(req.body);
+    req.on('data', reqBody => {
+        // Prin req.on("data") poti sa citesti din body-ul requestului, deci 
+        // daca ai scrie in afara acestei functii req.body.username nu o sa iti mearga
+        // O sa treabuiasca sa muti user.login() aici, si in loc de req.body.username folosesti JSON.parse(reqBody).username (poti sa il salvezi intr-o variabila daca ti-e mai usor)
 
+        /**
+         * If you want to use reqBody, you need to parse it from json to javascript object with JSON.parse(reqBody)
+         * Ex: // console.log(JSON.parse(reqBody).username);
+         */
+        console.log(`Data recive in body: ${reqBody}`)  
+        console.log("Username: "+JSON.parse(reqBody).username);
+      });
     user.login(req.body.username, req.body.password, function(result) {
         if(result) {
             res.send('Logged in as : ' + result.username);
