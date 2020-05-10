6452299aaafe7a40a528f73c23f42e061081c2d4
Catalina Gligor <catalinaanamaria56@gmail.com>
2020-05-10 19:37:13 +0300

    .

--- public/backend-js/practice-ppter.js
+++ public/backend-js/practice-ppter.js
@@ -24,7 +24,7 @@ let data={
 	email : "catalinaanamaria56@gmail.com",
 	parola : "123456789Catalina",
   titlu : "aaaa",
-  categorie: 'Auto',
+  categorie: ['Auto', 'Autoturisme', 'Cadillac'],
   descriere : "....",
   oras : "Arad",
   nrTel : "074679890"
@@ -57,33 +57,25 @@ async function olx(){
     ]);
   
     try{
-      await page.waitForSelector('#smsVerificationContainer > div.step.step1.active', {timeout : 5000});
+      await page.waitForSelector('#smsVerificationContainer > div.step.step1.active',{timeout : 10000});
       await page.click('#fancybox-close');
     } catch (error){
       console.log("Fancy box1 did not appear");
     }
     try{
-      await page.waitForSelector('#smsVerificationContainer > div.step.closeConfirm.active', {timeout : 5000})
-      await page.click('#fancybox-close');
+      await page.waitForSelector('#smsVerificationContainer > div.step.closeConfirm.active', {timeout : 10000})
+      await page.click('#smsVerificationContainer > div.step.closeConfirm.active > div.smsverification__footer > div.action-minor > a > span');
     }catch(error){
       console.log("Fancy box 2 did not appear");
     }
 
     await page.type('#add-title', data["titlu"]);
+
     await page.click('#targetrenderSelect1-0 > dt > a');
-    /* alegere categorie :((
-    switch(data["categorie"]){
-      case "Auto":
-        await page.click("#cat-5 > span.caticon.cat-icon-5");
-        break;
-      case "Imobiliare":
-          await page.click("#cat-3 > span.caticon.cat-icon-3");
-          break;
-       case "Locuri de munca":
-          await page.click("#cat-4 > span.caticon.cat-icon-4");
-          break;
-    }
-*/
+    await page.waitForSelector('#fancybox-content', {timeout: 10000});
+   
+
+    
     await page.type('#add-description', data["descriere"]);
 
    
@@ -98,12 +90,13 @@ async function olx(){
     
     //Posting
     //await page.click('#save');
+    //waits to take a screenshot of the page
     await timeout(5000);
     await page.screenshot({path: 'olx.png', fullPage: true});
     await browser.close();
 
 }
-//olx();
+olx();
 async function publi24(){
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -119,4 +112,4 @@ async function publi24(){
   await page.screenshot({path: 'publi.png', fullPage: true});
   await browser.close();
 }
-publi24();
+//publi24();
